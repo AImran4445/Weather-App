67a08f44377dd02683f288f164163b2e7241a3cf
“[firstname <aliimran98887@gmail.com>
2023-03-26 15:41:36 -1200

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "022520ebbc2f7885e41dcbbf80df7fe1",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
